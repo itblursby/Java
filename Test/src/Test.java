6d249bc9e04893d9e869db53a5c41ef024a80b8e
itblursby <morriswu65@gmail.com>
2021-01-09 09:10:29 -0800

    delicious

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		System.out.println("you entered: " + input);
+		//System.out.print("\u001B[H");
+		System.out.print("\u001B[40mblack\u001B[0m\n");
+		System.out.print("\u001B[41mred\u001B[0m\n");
+		System.out.print("\u001B[42mgreen\u001B[0m\n");
+		System.out.print("\u001B[43myellow\u001B[0m\n");
+		System.out.print("\u001B[44mblue\u001B[0m\n");
+		System.out.print("\u001B[45mmagenta\u001B[0m\n");
+		System.out.print("\u001B[0m"+"reset");
+		System.out.print("\n");
 	}
 
 }
